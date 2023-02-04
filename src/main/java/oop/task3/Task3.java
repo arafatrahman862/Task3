f776f84582db1d9f94d26c3d043c732ad3c68861
Arafat <sakib15-5422@diu.edu.bd>
2023-02-05 01:09:42 +0600

    "Task3"

---
+++ src/main/java/oop/task3/Task3.java
@@ -0,0 +1,20 @@
+package oop.task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name : ");
+        String name = input.nextLine();
+
+    
+
+        System.out.println("\nYour Information");
+        System.out.println("Name : " + name);
+      
+
+    }
+}
